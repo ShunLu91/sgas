de7e5d44e8847a5f06b75dbf775e83e0581a70d9
lushun_mac <470651748@qq.com>
2020-06-30 17:19:25 +0800

    data

--- cnn/run.sh
+++ cnn/run.sh
@@ -1,2 +1,3 @@
-CUDA_VISIBLE_DEVICES=3 python -u train_search.py --seed 0
-#CUDA_VISIBLE_DEVICES=3 nohup python -u train_search.py --note ask --seed 0 > logdir/p_ask_0.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=3 python -u train_search.py --seed 0
+CUDA_VISIBLE_DEVICES=2 nohup python -u train_search.py --seed 0 > logdir/rsg_0.log  2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup python -u train_search.py --seed 1 > logdir/rsg_1.log  2>&1 &
